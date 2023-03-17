00cddb5746d9369fc7eca4ff03ffd339dd2ab696
SeoSangwon <126433959+tkd8973@users.noreply.github.com>
2023-03-17 09:11:10 +0900

    del seaborn

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import seaborn as sns
 import plotly.express as px
 from plotly.subplots import make_subplots
 import plotly.graph_objs as go
